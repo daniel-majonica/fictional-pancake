d7fc06f488e4b2e1a6635d6a7254875b2241ee0a
Daniel Majonica <dmajonica@aol.com>
2022-02-17 11:43:14 +0100

    fixes indentation error

--- documentation.py
+++ documentation.py
@@ -1,18 +1,18 @@
- import pandas as pd
+import pandas as pd
  
- def return_column_headers(data_sheet, print_columns=False):
- """
-    Reads the data of an excel sheet and returns the names of each column. 
-    Optionally prints all names in the console in new lines.
-    
-    Args:
-        data_sheet : Excel sheet data
-        print_columns (optional): should names be printed in the console
-    Returns:
-        returns the names of each column as a list
- """
+def return_column_headers(data_sheet, print_columns=False):
+    """
+        Reads the data of an excel sheet and returns the names of each column. 
+        Optionally prints all names in the console in new lines.
+
+        Args:
+            data_sheet : Excel sheet data
+            print_columns (optional): should names be printed in the console
+        Returns:
+            returns the names of each column as a list
+    """
     b = pd.read_excel(data_sheet)
     column_headers = list(b.columns.values)
     if print_columns:
-        print("\n".join(column_headers))
+       print("\n".join(column_headers))
     return column_headers
